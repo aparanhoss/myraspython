8fc045ab0e3685af51e908b9636446cb0f1e5ca8
aparanhoss <aparanhoss@gmail.com>
2015-08-14 12:01:16 +0000

    Added object to retain data from serial

--- thSerial.py
+++ thSerial.py
@@ -9,4 +9,8 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 GPIO.setup(18,GPIO.OUT)
 
+
 ser = serial.Serial("/dev/ttyAMA0", baudrate = 9600, timeout = 2)
+
+#this object will contain data received from serial input
+data_rcv=Queue.Queue()
